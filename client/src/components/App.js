69f80f3cbb83587542412491a762f516c39af4e8
Michael Gee <michaelgee221@gmail.com>
2018-06-14 00:43:59 -0400

    src folder restructure - Header & Footer components added

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+
+      <h2>Bears Team 18</h2>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
