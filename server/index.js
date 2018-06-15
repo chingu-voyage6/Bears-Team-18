ba3dd0cf3aa9c4398c2bdaee221ff9bd0c6de012
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-06-15 01:49:42 -0400

    added winston test to api test

--- server/index.js
+++ server/index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
-const {makeExecutableSchema} = require('graphql-tools');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const morgan = require('morgan');
+const logger = require('./config/logger');
 const typeDefs = require('./graphql/schema');
 
 const app = express();
 
 const port = process.env.PORT ? process.env.PORT : 5000;
 
-const schema = makeExecutableSchema({typeDefs});
+process.env.NODE_ENV === 'production'
+  ? app.use(morgan('combined'))
+  : app.use(morgan('dev'));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+const schema = makeExecutableSchema({ typeDefs });
 
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.use('/api/test', (req, res, next) => {
+  logger.silly('test');
+  logger.warn('test');
   res.send('Connection to Back-End successful!');
 });
 
