27bd04f811f8031fcf25e64d1b6445714c4b32ea
Michael Gee <michaelgee221@gmail.com>
2018-06-16 16:15:08 -0400

    Material-UI Theme/Global Styling implemented - favicon changed - Google fonts added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  withStyles,
+} from '@material-ui/core/styles';
+
 import Header from './Header';
 import Footer from './Footer';
 
@@ -9,10 +15,33 @@ import Auth from './Routes/Auth';
 import UserDashboard from './Routes/UserDashboard';
 import NotFound from './Routes/NotFound';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0f143a',
+    },
+    secondary: {
+      main: '#15df89',
+    },
+  },
+});
+
+const styles = {
+  '@global': {
+    body: {
+      margin: 0,
+      padding: 0,
+      fontSize: '62.5%',
+      fontFamily: 'Roboto, Arial, Helvetica, sans-serif',
+    },
+  },
+};
+
 const App = () => {
   return (
     <Router>
-      <div>
+      <MuiThemeProvider theme={theme}>
         <Header />
 
         <Switch>
@@ -24,9 +53,9 @@ const App = () => {
         </Switch>
 
         <Footer />
-      </div>
+      </MuiThemeProvider>
     </Router>
   );
 };
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
