1bc36ed9ad1ecbc56d1d0f9f51abd8f37d72eeb9
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-06-17 15:31:01 -0400

    added mongoose and a couple of auth routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
+const { config } = require('dotenv');
+config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const logger = require('./config/logger');
 const typeDefs = require('./graphql/schema');
+const passport = require('passport');
+const queryString = require('querystring');
+const { signUserToken, verifyUserToken } = require('./lib/crypt');
+const User = require('./mongoose/user');
+require('./config/passport');
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    logger.silly('Database Connected');
+  })
+  .catch(err => logger.error(`Database Error ${err}`));
 
 const port = process.env.PORT ? process.env.PORT : 5000;
 
@@ -16,16 +31,58 @@ process.env.NODE_ENV === 'production'
 
 const schema = makeExecutableSchema({ typeDefs });
 
+app.use(passport.initialize());
+
+app.get(
+  '/auth/github',
+  passport.authenticate('github', {
+    scope: 'user',
+  })
+);
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { session: false }),
+  async (req, res) => {
+    const token = await signUserToken(req.user.id);
+    const query = queryString.stringify({
+      token,
+    });
+    res.redirect('/path-to-component?' + query);
+  }
+);
+
+app.use(async (req, res, next) => {
+  let token = req.headers.authorization;
+  if (token) {
+    try {
+      let userId = await verifyUserToken(token);
+      let user = await User.findById(userId);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next();
+      }
+    } catch (e) {
+      console.log(e);
+      logger.error(e);
+    }
+  } else {
+    next();
+  }
+});
+
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.use('/api/test', (req, res, next) => {
+  console.log(req.user);
   logger.silly('test');
-  logger.warn('test');
   res.send('Connection to Back-End successful!');
 });
 
 app.listen(port, () => {
-  console.log('Express Server Listening on Port 5000!');
+  logger.silly('Express Server Listening on Port 5000!');
 });
