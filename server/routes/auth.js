91aec1543674a7dabdac74ffb6c41bb171c71ab1
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-06-20 02:29:56 -0400

    auth routes init

---
+++ server/routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const logger = require('../services/logger');
+const User = require('../mongoose/user');
+const { signUserToken } = require('../services/crypt');
+const passport = require('passport');
+const queryString = require('querystring');
+const {
+  checkUser,
+  checkForUsernameAndPassword,
+  verifyPassword,
+} = require('../lib/mongoose-helper');
+const {
+  NoUserFound,
+  MissingUsernameOrPassword,
+  InvalidUsernameOrPassword,
+} = require('../lib/errors');
+const router = express.Router();
+
+router.post('/admin/login', async (req, res) => {
+  try {
+    checkForUsernameAndPassword(req.body.username, req.body.password);
+    let user = await checkUser({ username: req.body.username });
+    await verifyPassword(user, req.body.password);
+    let userToken = await signUserToken(user.id);
+    res.json({ token: userToken }).status(200);
+  } catch (e) {
+    if (e instanceof MissingUsernameOrPassword) {
+      res.json({ error: 'Username or password was not provided' }).status(400);
+    } else if (e instanceof NoUserFound) {
+      res.json({ error: 'Invalid username or password' }).status(400);
+    } else if (e instanceof InvalidUsernameOrPassword)
+      res.json({ error: 'Invalid username or password' }).status(400);
+    else {
+      res.status(500);
+      logger.error(e);
+    }
+  }
+});
+
+router.get(
+  '/github',
+  passport.authenticate('github', {
+    scope: 'user',
+  })
+);
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', { session: false }),
+  async (req, res) => {
+    const token = await signUserToken(req.user.id);
+    const query = queryString.stringify({
+      token,
+    });
+    res.redirect('/path-to-component?' + query);
+  }
+);
+module.exports = router;
