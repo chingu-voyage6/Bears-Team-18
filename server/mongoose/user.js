d5adf0420ead80155d020fe09fd4622c589c7411
Sloan Gwaltney <gwaltneys2@winthrop.edu>
2018-06-20 02:29:05 -0400

    added 2 keys to user model

--- server/mongoose/user.js
+++ server/mongoose/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
   githubId: String,
   permission: String,
   signUpComplete: Boolean,
