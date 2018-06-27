68d6739762a4ded5eecf0ed938b2752145deeb99
Unknown <frederickmorin18@gmail.com>
2018-06-27 02:06:57 -0400

    Make Header responsive, Add Raleway 700

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,29 +5,38 @@ import Button from '@material-ui/core/Button';
 
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
+const styles = ({ palette, breakpoints, typography }) => ({
   headerContainer: {
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: palette.primary.main,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    height: '64px',
-    padding: '0 24px',
+    minHeight: '64px',
+    padding: '0 32px',
+    [breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      minHeight: '96px',
+      paddingTop: '8px',
+    },
   },
   titleLink: {
     color: 'white',
     textDecoration: 'none',
+    textTransform: 'uppercase',
+    fontFamily: typography.title.fontFamily,
+    fontSize: '2rem',
+    fontWeight: 700,
+    [breakpoints.up('sm')]: {
+      marginLeft: '16px',
+    },
   },
-  headerTitle: {
-    fontFamily: theme.typography.title.fontFamily,
-    fontSize: '3.2em',
-  },
-  headerLink: {
-    margin: '0 16px',
-    color: '#fff',
 
+  headerLink: {
+    color: '#fff',
+    fontSize: '1.25rem',
     '&:hover': {
-      color: theme.palette.secondary.main,
+      color: palette.secondary.main,
     },
   },
 });
@@ -38,26 +47,25 @@ const Header = props => {
   return (
     <header className={classes.headerContainer}>
       <Link className={classes.titleLink} to="/">
-        <h1 className={classes.headerTitle}>CHINGU</h1>
+        CHINGU
       </Link>
 
       <div>
         <Button
-          size="small"
-          disableRipple={true}
           className={classes.headerLink}
           href="https://medium.com/chingu"
           target="_blank"
           rel="noopener noreferrer"
+          disableRipple
         >
           Blog
         </Button>
+
         <Button
-          size="small"
-          disableRipple={true}
           component={Link}
           to="/login"
           className={classes.headerLink}
+          disableRipple
         >
           Log In
         </Button>
